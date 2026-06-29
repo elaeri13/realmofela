1628f31c063467c2233e60b7faed8f0670b9c117
elaeri13 <130512985+elaeri13@users.noreply.github.com>
2026-06-29 16:35:34 -0500

    removal

--- serve.py
+++
@@ -1,8 +0,0 @@
-import os, sys
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-import http.server, socketserver
-PORT = 5173
-handler = http.server.SimpleHTTPRequestHandler
-with socketserver.TCPServer(("0.0.0.0", PORT), handler) as httpd:
-    print(f"Serving at http://localhost:{PORT}")
-    httpd.serve_forever()
